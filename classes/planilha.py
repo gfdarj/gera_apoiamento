c46b37601243c443ad8bd6de54e9821b97a4056c
gfdarj <gfdarj@gmail.com>
2025-08-17 23:45:53 -0300

    criando a estrutura do projeto

--- classes/planilha.py
+++ classes/planilha.py
@@ -2,8 +2,11 @@ from aplicacao import Configuracao
 import openpyxl
 
 class PlanilhaProjetos:
-    config = Configuracao()
+    def __init__(self):
+        self.config = Configuracao()
 
-    xls = openpyxl.load_workbook(config.arquivo_planilha_de_projetos, keep_vba=True)
-
+    def AbrePlanilha(self):
+        xls = openpyxl.load_workbook(self.config.arquivo_planilha_de_projetos, keep_vba=True)
+        xls.active = self.config.planilha_de_projetos
+        return xls
 
